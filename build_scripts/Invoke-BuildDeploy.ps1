08b9f706a8cad6df9bc28bd9675081eaf394aabb
Kirill Zaslavskii <kirzas@gmail.com>
2024-01-02 15:24:31 +0100

    add deployment to the test world

--- build_scripts/Invoke-BuildDeploy.ps1
+++ build_scripts/Invoke-BuildDeploy.ps1
@@ -1,4 +1,10 @@
 $minecraftPath = 'C:\Users\Argel\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang'
+$worldPath = 'C:\Users\Argel\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds\TestWorld'
+
+$destinations = @(
+    $minecraftPath
+    $worldPath
+)
 
 $repositoryPath = 'C:\Users\Argel\Dev\MinecraftGoblinsMod'
 
@@ -20,15 +26,15 @@ $buildFolderPath = Join-Path -Path $repositoryPath -ChildPath $buildFolderRelati
 if (-not (Test-Path -Path $buildFolderPath)) {
     throw "Build folder $buildFolderPath does not exist"
 }
-
-foreach ($type in $packTypes) {
-    $sourcePackTypeSubfolderPath = Join-Path -Path $repositoryPath -ChildPath $buildFolderRelativePath -AdditionalChildPath "${type}_$packName.mcpack"
+foreach ($destination in $destinations) {
+    foreach ($type in $packTypes) {
+        $sourcePackTypeSubfolderPath = Join-Path -Path $repositoryPath -ChildPath $buildFolderRelativePath -AdditionalChildPath "${type}_$packName.mcpack"
     
-    
-    $destinationPackTypeSubfolderPath = Join-Path -Path $minecraftPath -ChildPath "${type}_packs"
-    $destinationPackTypeFullPath = Join-Path -Path $destinationPackTypeSubfolderPath -ChildPath $packName
+        $destinationPackTypeSubfolderPath = Join-Path -Path $destination -ChildPath "${type}_packs"
+        $destinationPackTypeFullPath = Join-Path -Path $destinationPackTypeSubfolderPath -ChildPath $packName
 
-    Remove-Item -Path $destinationPackTypeFullPath -Force -Recurse
+        Remove-Item -Path $destinationPackTypeFullPath -Force -Recurse
 
-    Expand-Archive -Path $sourcePackTypeSubfolderPath -Destination $destinationPackTypeFullPath -Force
+        Expand-Archive -Path $sourcePackTypeSubfolderPath -Destination $destinationPackTypeFullPath -Force
+    }
 }
